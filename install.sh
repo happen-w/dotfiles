0def353a26f82e4108a33b2a338790a017f2d6a1
happen <1592578957@qq.com>
2024-04-19 08:41:32 +0000

    修改install.sh

--- install.sh
+++ install.sh
@@ -3,41 +3,63 @@
 scriptDir=$(cd `dirname $0`; pwd)
 . "$scriptDir/public/index.sh"
 
-cp /etc/apt/sources.list /etc/apt/sources.list.bak
-sed -i "s@http://.*archive.ubuntu.com@http://mirrors.tuna.tsinghua.edu.cn@g" /etc/apt/sources.list
-sed -i "s@http://.*security.ubuntu.com@http://mirrors.tuna.tsinghua.edu.cn@g" /etc/apt/sources.list
-apt update
+#cp /etc/apt/sources.list /etc/apt/sources.list.bak
+#sed -i "s@http://.*archive.ubuntu.com@http://mirrors.tuna.tsinghua.edu.cn@g" /etc/apt/sources.list
+#sed -i "s@http://.*security.ubuntu.com@http://mirrors.tuna.tsinghua.edu.cn@g" /etc/apt/sources.list
+#apt update
 
-install_list=(vim curl zsh git ripgrep net-tools unzip nodejs npm wget tldr gdb cmake)
+install_list=(vim curl zsh git ripgrep net-tools unzip nodejs npm wget gdb cmake cland-12)
+
+echo ${install_list[@]} 
 
 for elem in ${install_list[@]}
 do
-  if ! isCmdExist $elem; then
-    echoInfo 'installing'$elem
-    apt install -y $elem
-    echoInfo 'intalled '$elem' done'
+  if ! isCmdExist $elem;then
+      echo 'installing '$elem
+      apt install -y $elem > /dev/null 2>&1 
+      if isCmdExist $elem;then
+          echo 'intalled '$elem' done'
+      else
+          echoError 'install '$elem' failed'
+      fi
+  else
+      echoWarn 'already intalled '$elem''
   fi
 done
 
-# echoInfo 'install neovim'
-# echo -e "\n" | add-apt-repository ppa:neovim-ppa/stable
-# apt-get update
-# apt-get install -y neovim
-# apt-get install -y python3-dev python3-pip
-# git config --global core.editor "vim"
-# mkdir -p ~/.config/nvim/
-# cp -R $scriptDir/nvim/* ~/.config/nvim/
+if ! isCmdExist nvim;then
+  echo 'installing nvim'
+  snap install nvim --classic
+  if isCmdExist nvim;then
+    echo 'installed nvim done'
+  else
+    echo 'installed nvim failed'
+  fi
+else
+  echoWarn 'already intalled nvim'
+fi
 
-# git config --global url."github".insteadOf kkgithub
 
-# git clone --depth=1 https://kgithub.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+#sh "$scriptDir/ohmyzsh.sh"
+#echo 'install plugin'
+#git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+#git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+#git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
-# git clone https://kgithub.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-# git clone https://kgithub.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-#
-#
-# python3 
-# ln -s python /usr/bin/python3.10
-#
-sudo pip3 install tldr
+
+
+if [ ! -d ~/.config/nvim/ ]; then
+   echo 'mkdir and copy nvim config'
+   mkdir -p  ~/.config/nvim/
+   rm -rf ~/.config/nvim
+   ln -s ~/dotfile-lazy ~/.config/nvim
+else
+   echo 'already has nvim config '
+fi
+
+
+#sudo apt install python3-pip
+#sudo pip install python-lsp-server
+#sudo pip install ipython
+
